60c196c2ed8e9ecc24dca0d0fefd538efaf29a62
Megan Horsfield <meganthadev@gmail.com>
2020-09-01 06:47:03 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -8,6 +8,11 @@ export default function manageBand(state = {
 
       const band= {id: uuid(), name: action.name};
       return { ...state, bands: [...state.bands, action.name] }
+      
+    case 'DELETE_BAND':
+      
+      const bands= state.bands.filter(band => band.id !== action.id)
+      return {bands};
 
     default:
       return state;
