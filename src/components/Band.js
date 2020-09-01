7e7742dd61b7c77fd3651a7ceec63d2de970f4f7
Megan Horsfield <meganthadev@gmail.com>
2020-09-01 06:47:48 +0000

    Automatically backed up by Learn

--- src/components/Band.js
+++ src/components/Band.js
@@ -9,7 +9,7 @@ class Band extends Component {
   render() {
     return(
       <div>
-        <li>{this.props.name}</li>
+      <li>{this.props.name}</li>
       <button onClick={this.handleOnClick}>Delete</button>
       </div>
     );
